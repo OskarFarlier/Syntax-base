1ab5559dd383de5ef0c85fffe267f879a9d137e8
OskarFarlier <43237862+OskarFarlier@users.noreply.github.com>
2019-08-30 22:31:00 +0600

    Create array_learn.php

---
+++ array_learn.php
@@ -0,0 +1,64 @@
+     <?php
+     // НАЧАЛО РЕШЕНИЕ ЗАДАЧ 
+
+        #Работа с count
+    // Дан массив $arr. Подсчитайте количество элементов этого массива.
+
+
+// $arr = ['e', 'd', 'c', 'b', 'a',];
+
+// echo "<br>";
+// echo count($arr);
+// echo "<br>";
+
+
+// Дан массив $arr. С помощью функции count выведите последний элемент данного массива.
+
+// $arr = ['e', 'd', 'c', 'b', 'a',];
+
+// echo "<br>";
+// echo $arr[count($arr)-1];
+// echo "<br>";
+
+
+#Работа с in_array
+
+
+// $arr = ['e', 'd', 'c', 'b', 'a', 3,];
+
+// echo "<br>";
+// echo in_array(3, $arr); // 1 Верно тоесть есть  0 значит нету
+
+#Работа с array_sum и array_product
+
+// $arr = [1, 2, 3, 4, 5];
+ 
+// echo array_sum($arr);
+// echo array_product($arr);
+// echo array_sum($arr)/count($arr);
+
+
+
+// $arr = range(1, 100); 
+// print_r($arr); 
+// echo "<br>";
+
+
+
+// $arr = range('a', 'z'); 
+// print_r($arr); 
+
+
+
+
+// $arr = [1, 2, 3, 4, 5]; 
+// array_unshift($arr, 0); 
+// array_push($arr, 6); 
+// print_r($arr); 
+
+
+// $str = '1234567890'; 
+// $arr = array_sum(str_split($str, 1)); 
+// print_r($arr); 
+
+?>
